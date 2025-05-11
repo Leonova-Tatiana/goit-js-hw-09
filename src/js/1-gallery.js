e86dce3faedb1207565de6e1f4bc02da52aa194f
Leonova-Tatiana <tanysha155@icloud.com>
2025-05-11 23:02:55 +0300

    mmm

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,5 +1,6 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import { preview } from 'vite';
 
 const images = [
   {
@@ -68,17 +69,21 @@ const images = [
 ];
 
 const galleryEl = document.querySelector('.gallery');
-
-function imageTemplate({ preview, original, description }) {
-  return `<li class="gallery-item">
-      <a class="gallery-link" href="${original}">
+function createGalleryMarkup(images) {
+  return images
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery-item">
+        <a class="gallery-link" href="${original}">
         <img
           class="gallery-image"
           src="${preview}"
           alt="${description}"
-        />
-      </a>
-    </li>`;
+       />
+  </a>
+</li>`
+    )
+    .join('');
 }
 
 function imagesTemplate(images) {
